3cc83df310e6b828b78d72bf1aa2ea44899c235d
Jade <jade0@hotmail.fr>
2015-10-06 08:39:12 +0200

    PONG

---
+++ Tutos/PONG/Assets/control.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+
+    public Transform Player1;
+    public Transform Player2;
+    public Camera MainCam;
+    public BoxCollider2D North;
+    public BoxCollider2D South;
+    public BoxCollider2D West;
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
