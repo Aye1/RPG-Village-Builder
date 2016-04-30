687784669991ee649666123b6649caeb1f8b1100
Adrien Janvier <janvier.adrien@gmail.com>
2016-04-30 22:12:48 +0200

    Gé

--- Tutos/JadrienLike/JadrienLike/Assets/Scripts/BoardManager.cs
+++ Tutos/JadrienLike/JadrienLike/Assets/Scripts/BoardManager.cs
@@ -4,18 +4,31 @@ using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour {
 	
-	public GameObject[] floorTiles;
-	public GameObject[] platformTiles;
-	public GameObject[] undergroundTiles;
+	//public GameObject[] floorTiles;
+	//public GameObject[] undergroundTiles;
+
+	public GameObject[] topTiles;
+	public GameObject[] bottomTiles;
+	public GameObject[] leftTiles;
+	public GameObject[] rightTiles;
+	public GameObject[] cornerbgTiles;
+	public GameObject[] cornerbdTiles;
+	public GameObject[] cornerhgTiles;
+	public GameObject[] cornerhdTiles;
+	public GameObject[] cliffbgTiles;
+	public GameObject[] cliffbdTiles;
+	public GameObject[] cliffhgTiles;
+	public GameObject[] cliffhdTiles;
+	public GameObject[] backgroundTiles;
 	
-	private int minX = 10;
-	private int maxX = 30;
-	private int minY = 1;
-	private int maxY = 1;
-	private int offsetX = 4;
+	private int minX = 20;
+	private int maxX = 40;
+	private int minY = 20;
+	private int maxY = 40;
+	private int offsetX = 1;
 	private int undergroundDepth = -10;
 	
-	private Transform boardHolder;
+	//private Transform boardHolder;
 	
 	// Use this for initialization
 	void Start () {
@@ -50,16 +63,36 @@ public class BoardManager : MonoBehaviour {
 		// On peut perdre une case de longueur, on s'en fout pour le moment
 		int boundX = board.SizeX / 2;
 		for (int x = -boundX; x < boundX ; x++) {
-			xOffset = x*offsetX;
-           
 			for (int y = 0; y < board.SizeY ; y++) {
-				GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
-				GameObject instance = Instantiate(toInstantiate, new Vector3(xOffset,y,0f), Quaternion.identity) as GameObject;
-				instance.transform.SetParent(boardHolder); 
-			}
-			for (int y = -2; y > undergroundDepth; y--) {
-				GameObject toInstantiate = undergroundTiles[Random.Range(0, undergroundTiles.Length)];
-				GameObject instance = Instantiate(toInstantiate, new Vector3(xOffset,y,0f), Quaternion.identity) as GameObject;
+				GameObject toInstantiate;
+				if (x == -boundX && y == 0)
+				{
+					toInstantiate = cornerbgTiles[Random.Range(0, cornerbgTiles.Length)];
+				} else if (x == boundX-1 && y == 0)
+				{
+					toInstantiate = cornerbdTiles[Random.Range(0, cornerbdTiles.Length)];
+				} else if (x == -boundX && y == board.SizeY-1)
+				{
+					toInstantiate = cornerhgTiles[Random.Range(0, cornerhgTiles.Length)];
+				} else if (x == boundX-1 && y == board.SizeY-1)
+				{
+					toInstantiate = cornerhdTiles[Random.Range(0, cornerhdTiles.Length)];
+				} else if (x == -boundX)
+				{
+					toInstantiate = leftTiles[Random.Range(0, leftTiles.Length)];
+				} else if (x == boundX-1)
+				{
+					toInstantiate = rightTiles[Random.Range(0, rightTiles.Length)];
+				} else if (y == 0)
+				{
+					toInstantiate = bottomTiles[Random.Range(0, bottomTiles.Length)];
+				} else if (y == board.SizeY-1)
+				{
+					toInstantiate = topTiles[Random.Range(0, topTiles.Length)];
+				} else {
+					toInstantiate = backgroundTiles[Random.Range(0, backgroundTiles.Length)];
+				}
+				GameObject instance = Instantiate(toInstantiate, new Vector3(x,y,0f), Quaternion.identity) as GameObject;
 				instance.transform.SetParent(boardHolder); 
 			}
 		}
