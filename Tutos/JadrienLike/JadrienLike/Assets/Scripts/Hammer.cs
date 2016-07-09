0b8cb3d0b6f92a29c63d9efd58209e4909040859
Adrien Janvier <janvier.adrien@gmail.com>
2016-07-09 21:54:07 +0200

    Hammer fonctionnel + aiguilles fixed

---
+++ Tutos/JadrienLike/JadrienLike/Assets/Scripts/Hammer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hammer : MonoBehaviour {
+
+    private Player _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = gameObject.GetComponentInParent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            _player.Attack(enemy, _player.weaponDamage);
+        }
+    }
+}
