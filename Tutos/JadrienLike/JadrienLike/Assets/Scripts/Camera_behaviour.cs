c1781ecf317fdb8038f094639e48919693baef14
Adrien Janvier <janvier.adrien@gmail.com>
2016-10-08 11:24:14 +0200

    Recalcul de la taille de la caméra seulement lorsque l'écran change

--- Tutos/JadrienLike/JadrienLike/Assets/Scripts/Camera_behaviour.cs
+++ Tutos/JadrienLike/JadrienLike/Assets/Scripts/Camera_behaviour.cs
@@ -9,54 +9,99 @@ public class Camera_behaviour : MonoBehaviour
     private Vector3 minResolution = new Vector3(1024f, 768f, 0);
     private float targetRatio = 16f / 9f;
 
+    private Camera _camera;
+    private int _width;
+    private int _height;
+
+    // Width of the camera
+    public int Width
+    {
+        get
+        {
+            return _width;
+        }
+
+        set
+        {
+            if (value != _width)
+            {
+                _width = value;
+                AdjustScreenScale();
+            }
+        }
+    }
+
+    // Height of the camera
+    public int Height
+    {
+        get
+        {
+            return _height;
+        }
+
+        set
+        {
+            if (value != _height)
+            {
+                _height = value;
+                AdjustScreenScale();
+            }
+        }
+    }
+
+
     // Use this for initialization
     void Start()
     {
         offset = transform.position; //- player.transform.position;
+        _camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-        AdjustScreenScale();
+        Width = Screen.width;
+        Height = Screen.height;
+        //AdjustScreenScale();
     }
 
+    /// <summary>
+    /// Computes the new scale of the screen
+    /// </summary>
     private void AdjustScreenScale()
     {
         // determine the game window's current aspect ratio
-        float windowaspect = (float)Screen.width / (float)Screen.height;
+        float windowaspect = Screen.width / (float)Screen.height;
 
         // current viewport height should be scaled by this amount
         float scaleheight = windowaspect / targetRatio;
 
-        // obtain camera component so we can modify its viewport
-        Camera camera = GetComponent<Camera>();
 
         // if scaled height is less than current height, add letterbox
         if (scaleheight < 1.0f)
         {
-            Rect rect = camera.rect;
+            Rect rect = _camera.rect;
 
             rect.width = 1.0f;
             rect.height = scaleheight;
             rect.x = 0;
             rect.y = (1.0f - scaleheight) / 2.0f;
 
-            camera.rect = rect;
+            _camera.rect = rect;
         }
         else // add pillarbox
         {
             float scalewidth = 1.0f / scaleheight;
 
-            Rect rect = camera.rect;
+            Rect rect = _camera.rect;
 
             rect.width = scalewidth;
             rect.height = 1.0f;
             rect.x = (1.0f - scalewidth) / 2.0f;
             rect.y = 0;
 
-            camera.rect = rect;
+            _camera.rect = rect;
         }
     }
 }
