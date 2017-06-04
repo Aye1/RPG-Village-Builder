dea9fe4bc214f1803715853aa17df064056001b4
Adrien Janvier <janvier.adrien@gmail.com>
2017-06-04 15:42:30 +0200

    TowerDefense step 1

---
+++ Tower Defense/Assets/Board.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool isPositionValid(Vector3 pos)
+    {
+        bool res = true;
+        Color pixelColor = GetComponentInChildren<SpriteRenderer>().sprite.texture.GetPixel((int)pos.x, (int)pos.y);
+        if (pixelColor == Color.white)
+        {
+            res = false;
+        }
+        return res;
+    }
+}
---
+++ Tower Defense/Assets/TowerTool.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTool : MonoBehaviour {
+
+    public BasicTower tower;
+    public Board board;
+
+    public enum ToolState { None, AddTower };
+    private ToolState toolState;
+    private BasicTower brushTower;
+
+    private readonly int gridSize = 20;
+
+	// Use this for initialization
+	void Start () {
+        brushTower = Instantiate(tower);
+        brushTower.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateBrushPosition();
+        updateBrushColor();
+        if (board != null)
+        {
+            if (Input.GetMouseButtonDown(0) && board.isPositionValid(Input.mousePosition))
+            {
+                BasicTower newTower = Instantiate(tower, board.transform);
+                Vector3 pos = Input.mousePosition;
+                pos = magnetPosToGrid(pos);
+                pos = Camera.main.ScreenToWorldPoint(pos);
+                pos.z = -1;
+                newTower.transform.localPosition = pos;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rotateTool();
+            }
+        }
+        tower.enabled = toolState == ToolState.AddTower;
+	}
+
+    private void updateBrushPosition()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos = magnetPosToGrid(pos);
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        pos.z = -1;
+        brushTower.transform.position = pos;
+    }
+
+    private void updateBrushColor()
+    {
+        if (board.isPositionValid(Input.mousePosition))
+        {
+            brushTower.GetComponentInChildren<SpriteRenderer>().color = Color.green;
+        } else
+        {
+            brushTower.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+        }
+    }
+
+    private void rotateTool()
+    {
+        if (toolState == ToolState.None)
+        {
+            toolState = ToolState.AddTower;
+        } else
+        {
+            toolState = ToolState.None;
+        }
+    }
+    private Vector3 magnetPosToGrid(Vector3 pos)
+    {
+        Vector3 res = pos;
+        res.x = ((int)res.x / gridSize) * gridSize + gridSize / 2;
+        res.y = ((int)res.y / gridSize) * gridSize + gridSize / 2;
+        return res;
+    }
+}
