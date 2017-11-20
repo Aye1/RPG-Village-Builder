08472b36587cdfba0a795fbec9dcac42838c0160
Adrien Janvier <janvier.adrien@gmail.com>
2017-11-20 22:43:00 +0100

    Multiple buildings adding

---
+++ RPG Village Builder/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+    public Building house;
+    public Building[] referenceBuildings;
+
+    public int currentBuildingIndex = 0;
+
+    private Dictionary<Vector3, Building> _buildings;
+
+    private const float yOffset = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+        _buildings = new Dictionary<Vector3, Building>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageKeys();
+	}
+
+    private void ManageKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeBuildingTool();
+        }
+    }
+
+    public bool IsPosOccupied(Vector3 pos)
+    {
+        return _buildings.ContainsKey(pos);
+    }
+
+    public void CreateBuilding(Vector3 pos)
+    {
+        if (CanBuildAtPos(pos))
+        {
+            Building referenceBuilding = referenceBuildings[currentBuildingIndex];
+            Building buildingCopy = Instantiate(referenceBuilding, transform);
+            buildingCopy.transform.position = new Vector3 (pos.x, yOffset, pos.z);
+            _buildings.Add(pos, buildingCopy);
+        }
+    }
+
+    public bool CanBuildAtPos(Vector3 pos)
+    {
+        return !IsPosOccupied(pos) && !IsYTooHigh(pos);
+    }
+
+    public bool IsYTooHigh(Vector3 pos)
+    {
+        return pos.y > 0.1f;
+    }
+
+    public void ChangeBuildingTool()
+    {
+        if (referenceBuildings.Length > 1)
+        {
+            currentBuildingIndex = (currentBuildingIndex + 1) % referenceBuildings.Length;
+        }
+    }
+
+    public Building GetSelectedBuilding()
+    {
+        return referenceBuildings[currentBuildingIndex];
+    }
+}
---
+++ RPG Village Builder/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+    public GameObject posIndicator;
+    private BuildingManager _buildingManager;
+
+    private Building _ghostBuilding;
+    private Building _toBeDestroyedBuilding;
+
+    private const float yOffset = 0.01f;
+    private int div = 20;
+
+	// Use this for initialization
+	void Start () {
+        _buildingManager = FindObjectOfType<BuildingManager>();
+        if (_buildingManager == null)
+        {
+            Debug.LogError("Building Manager not found.");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePosIndicator();
+        ManageClick();
+	}
+
+    private void UpdatePosIndicator()
+    {
+        Vector3 hitPosition = GetMousePositionInWorld();
+        if (hitPosition != Vector3.zero) {
+            Vector3 pos = GetDiscreteMousePosition();
+            posIndicator.transform.position = new Vector3(pos.x, yOffset, pos.z);
+            UpdateIndicatorColor(pos);
+            UpdateGhostBuilding(pos);
+        }
+    }
+
+    private void UpdateGhostBuilding(Vector3 pos)
+    {
+        Building currentBuilding = _buildingManager.GetSelectedBuilding();
+        if (_ghostBuilding == null || (currentBuilding.GetType() != _ghostBuilding.GetType()))
+        {
+            if (_ghostBuilding != null)
+            {
+                Destroy(_ghostBuilding.gameObject);
+            }
+            _ghostBuilding = Instantiate(currentBuilding, transform);
+        }
+        _ghostBuilding.transform.position = pos;
+    }
+
+    private Vector3 GetMousePositionInWorld()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            return hit.point;
+        } 
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+    
+    private Vector3 GetDiscreteMousePosition()
+    {
+        Vector3 mousePos = GetMousePositionInWorld();
+        if (mousePos == Vector3.zero)
+        {
+            return Vector3.zero;
+        }
+        return new Vector3(DiscreteCoord(mousePos.x, div) + div / 2, DiscreteCoord(mousePos.y, div), DiscreteCoord(mousePos.z, div));
+    }
+
+    private float DiscreteCoord(float coord, int div)
+    {
+        return (int)(coord / div) * div;
+    } 
+
+    private void ManageClick()
+    {
+        if(Input.GetMouseButtonDown(0) && GetMousePositionInWorld().y <= 0.1f)
+        {
+            _buildingManager.CreateBuilding(GetDiscreteMousePosition());
+        }
+    }
+
+    private void UpdateIndicatorColor(Vector3 pos)
+    {
+        Color color = Color.red;
+        if (_buildingManager.CanBuildAtPos(pos))
+        {
+            color = Color.green;
+        }
+        posIndicator.GetComponentInChildren<SpriteRenderer>().color = color;
+    }
+}
