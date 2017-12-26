181091231a3fc9550ca93f5e7957b2f0921da10f
Adrien Janvier <janvier.adrien@gmail.com>
2017-12-26 13:23:56 +0100

    Clean other projects

--- JadrienLike/Assets/Scripts/Constantes.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class Constantes {
-
-    public const int mentalUp = 50;
-    public const int mentalDown = -50;
-}
--- JadrienLike/Assets/Scripts/Constants/TagConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿public static class TagConstants
-{
-    public static readonly string TagHat = "hat";
-    public static readonly string TagPlayer = "Player";
-    public static readonly string TagEnemy = "Enemy";
-    public static readonly string TagDestructible = "Destructible";
-}
